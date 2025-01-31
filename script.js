1856408b6dfc03b2183fd4f1734b98d7ff149f1e
Ajinkya Chavan <72398926+FrozenFalcon-Byte@users.noreply.github.com>
2025-01-31 13:28:28 +0530

    resoponsivness and light mode added

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const videos = document.querySelectorAll("[data-video]");
 
-    videos.forEach((video) => {
-        gsap.to(video, {
-            scrollTrigger: {
-                trigger: video,
-                start: "top 80%", 
-                end: "bottom 20%",
-                onEnter: () => video.play(),
-                onLeave: () => video.pause(),
-                onEnterBack: () => video.play(),
-                onLeaveBack: () => video.pause(),
-                toggleActions: "play none none none"
-            }
-        });
-    });
-});
 
 
 
@@ -42,22 +25,45 @@ tl1.to(".moving-text", {
     opacity: 1,
 });
 
-gsap.utils.toArray('.zoom-div').forEach((zoomDiv) => {
-    let modelContainer = zoomDiv.querySelector('.model-container');
-
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: zoomDiv,
-            pin: true,
-            start: "top",
-            end: "90% 30%",
-            scrub: 1,
-            toggleActions: "play reverse play reset",
-        },
-    }).to(modelContainer, {
-        width: "100%",
+if (window.matchMedia("(min-width: 768px)").matches) {
+    // Adjust breakpoint as needed
+    document.addEventListener("DOMContentLoaded", () => {
+        const videos = document.querySelectorAll("[data-video]");
+    
+        videos.forEach((video) => {
+            gsap.to(video, {
+                scrollTrigger: {
+                    trigger: video,
+                    start: "top 80%", 
+                    end: "bottom 20%",
+                    onEnter: () => video.play(),
+                    onLeave: () => video.pause(),
+                    onEnterBack: () => video.play(),
+                    onLeaveBack: () => video.pause(),
+                    toggleActions: "play none none none"
+                }
+            });
+        });
     });
-});
+
+    gsap.utils.toArray('.zoom-div').forEach((zoomDiv) => {
+        let modelContainer = zoomDiv.querySelector('.model-container');
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: zoomDiv,
+                pin: true,
+                start: "top",
+                end: "90% 30%",
+                scrub: 1,
+                toggleActions: "play reverse play reset",
+            },
+        }).to(modelContainer, {
+            width: "100%",
+        });
+    });
+}
+
 
 var tl3 = gsap.timeline({
     scrollTrigger: {
@@ -80,3 +86,16 @@ tl3.to("#experience img", {
 }, "animation").to("#exp-container h1", {
     y:"-10%",
 }, "animation");
+
+
+
+function toggleLightMode() {
+    let isLight = document.body.classList.toggle('light-mode');
+    localStorage.setItem('lightMode', isLight ? 'enabled' : 'disabled');
+  }
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('lightMode') === 'enabled') {
+      document.body.classList.add('light-mode');
+    }
+  });
